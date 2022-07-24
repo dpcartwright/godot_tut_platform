6070beb48ef9400c18de1b0b6c5be022f7012aa8
David Cartwright <thefrenchtart@gmail.com>
2022-07-24 15:43:46 +0100

    end of p4

--- Player.gd
+++ Player.gd
@@ -1,8 +1,19 @@
 extends KinematicBody2D
 
-export(int) var JUMP_FORCE = -130
+export(int) var JUMP_FORCE = -160
+export(int) var JUMP_RELEASE_FORCE = -40
+export(int) var MAX_SPEED = 75
+export(int) var ACCELERATION = 10
+export(int) var FRICTION = 10
+export(int) var GRAVITY = 5
+export(int) var ADDITIONAL_FALL_GRAVITY = 2
 
 var velocity = Vector2.ZERO
+
+onready var animatedSprite = $AnimatedSprite
+
+func _ready():
+	animatedSprite.frames = load("res://PlayerBlueSkin.tres")
 
 func _physics_process(delta):
 	apply_gravity()
@@ -11,26 +22,39 @@ func _physics_process(delta):
 	
 	if input.x == 0:
 		apply_friction()
+		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
-			
+		animatedSprite.animation = "Run"
+		
+		if input.x > 0:
+			animatedSprite.flip_h = true
+		else:
+			animatedSprite.flip_h = false
+
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
 	else:
-		if Input.is_action_just_released("ui_up") and velocity.y < -70:
-			velocity.y = -70
+		animatedSprite.animation = "Jump"
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
 
 		if velocity.y > 0:
-			velocity.y += 4
+			velocity.y += ADDITIONAL_FALL_GRAVITY
 
+	var was_in_air = !is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	if is_on_floor() == was_in_air:
+		animatedSprite.animation = "Run"
+		animatedSprite.frame = 1
 
 func apply_gravity():
-	velocity.y += 4
+	velocity.y += GRAVITY
+	velocity.y = min(velocity.y, 300)
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, 10)
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, 50 * amount, 10)
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
