eb1c10ee81f6a7567459d09cefa98ebd00850f65
David Cartwright <thefrenchtart@gmail.com>
2022-08-01 12:58:39 +0100

    end of p8

--- Player.gd
+++ Player.gd
@@ -1,20 +1,37 @@
 extends KinematicBody2D
 class_name Player
 
+enum {
+	MOVE,
+	CLIMB
+}
+
 export(Resource) var moveData
 
 var velocity = Vector2.ZERO
+var state = MOVE
 
 onready var animatedSprite = $AnimatedSprite
+onready var ladderCheck = $LadderCheck
 
 func _ready():
 	animatedSprite.frames = load("res://PlayerBlueSkin.tres")
 
-func _physics_process(delta):
-	apply_gravity()
+func _physics_process(delta):	
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
 	
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+	
+
+func move_state(input):
+	if  is_on_ladder() && Input.is_action_pressed("ui_up"):
+		state = CLIMB
+		
+	apply_gravity()
 	if input.x == 0:
 		apply_friction()
 		animatedSprite.animation = "Idle"
@@ -43,6 +60,23 @@ func _physics_process(delta):
 	if is_on_floor() == was_in_air:
 		animatedSprite.animation = "Run"
 		animatedSprite.frame = 1
+	
+func climb_state(input):
+	if !is_on_ladder(): state = MOVE
+	if input.length() != 0:
+		animatedSprite.animation = "Run"
+	else:
+		animatedSprite.animation = "Idle"
+		
+	velocity = input * 50
+	velocity = move_and_slide(velocity, Vector2.UP)
+	pass
+
+func is_on_ladder():
+	if !ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if !collider is Ladder: return false
+	return true
 
 func apply_gravity():
 	velocity.y += moveData.GRAVITY
